935a2707705b67146b912ab0d1c3ec260d0c18e5
Alisher Baisaganov <baisaganov99@gmail.com>
2025-10-13 16:41:59 +0500

    fix init in base_page

--- base/__init__.py
+++ base/__init__.py
@@ -2,7 +2,7 @@ from enum import Enum
 import configparser
 import logging as log
 import re
-from services.egov.sign_service import SignXml
+# from services.egov.sign_service import SignXml
 from playwright.sync_api import Page
 import allure
 
